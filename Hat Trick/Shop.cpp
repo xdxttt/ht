66e5b7278aa0a1caa3724aa332ce71b47412756b
xdxttt <xdxttt@gmail.com>
2012-10-22 08:33:59 +0800

    add shop

---
+++ Hat Trick/Shop.cpp
@@ -0,0 +1,9 @@
+//
+//  shop.cpp
+//  Hat Trick
+//
+//  Created by xudexin on 12-10-22.
+//
+//
+
+#include "Shop.h"
---
+++ Hat Trick/Shop.h
@@ -0,0 +1,14 @@
+//
+//  shop.h
+//  Hat Trick
+//
+//  Created by xudexin on 12-10-22.
+//
+//
+
+#ifndef __Hat_Trick__shop__
+#define __Hat_Trick__shop__
+
+#include <iostream>
+
+#endif /* defined(__Hat_Trick__shop__) */
